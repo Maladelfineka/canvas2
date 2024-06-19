af70edec16e7975135c45ad8de7c4b6bbbd5c52b
Ola <129540485+Maladelfineka@users.noreply.github.com>
2024-06-19 17:26:02 +0200

    Add files via upload

--- script.js
+++ script.js
@@ -4,11 +4,11 @@ let canvas = document.querySelector('#canvas');
 console.log(canvas);
 
 var ctx = canvas.getContext('2d');
-ctx.fillStyle = "purple";
-ctx.fillRect(100,100,50,50)
+ctx.fillStyle = "black";
+ctx.fillRect(50,50,50,50)
 
-ctx.fillStyle = "red";
-ctx.fillRect(50,50,25,50)
+ctx.fillStyle = "green";
+ctx.fillRect(150,150,25,50)
 
 var centerX = canvas.width / 2;
 var centerY = canvas.height / 2;
@@ -16,7 +16,20 @@ var radius = 70;
 ctx.beginPath();
 
 ctx.arc(centerX, centerY, radius, 0, 2 * Math.PI, false);
-ctx.fillStyle = 'pink'
+ctx.fillStyle = 'blue'
 ctx.fill()
-ctx.strokeStyle = 'pink'
+ctx.strokeStyle = 'blue'
 ctx.stroke();
+
+ctx.fillStyle = "red";
+ctx.fillRect(450,450,50,50)
+
+ctx.fillStyle = "orange";
+ctx.fillRect(0,450,55,50)
+
+ctx.fillStyle = "yellow";
+ctx.fillRect(450, 0,55,50)
+
+
+ctx.fillStyle = "grey";
+ctx.fillRect(0,0,55,50)
